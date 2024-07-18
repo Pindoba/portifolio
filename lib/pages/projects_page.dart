90b5b7891227db5685aa3faf7fdff8b50f241847
Welton Moura <weltonmoura@live.com>
2024-07-18 10:48:11 -0300

    navegacao projetos

--- lib/pages/projects_page.dart
+++ lib/pages/projects_page.dart
@@ -2,26 +2,95 @@ import 'package:flutter/material.dart';
 import 'package:portifolio/repository/project_repository.dart';
 import 'package:portifolio/widgets/project_widget.dart';
 
-class ProjectsPage extends StatelessWidget {
+class ProjectsPage extends StatefulWidget {
   final bool mobile;
   const ProjectsPage({super.key, required this.mobile});
 
   @override
+  State<ProjectsPage> createState() => _ProjectsPageState();
+}
+
+class _ProjectsPageState extends State<ProjectsPage> {
+    int _indice = 0;
+    void _incrementIndice() {
+      setState(() {
+        _indice++;
+      });
+    }
+    void _decrementIndice() {
+      setState(() {
+        _indice--;
+      });
+    }
+  @override
   Widget build(BuildContext context) {
     final correntWidth = MediaQuery.of(context).size.width;
+
     return Container(
-      padding: EdgeInsets.all(10),
+      padding: const EdgeInsets.all(10),
       color: Colors.brown,
       child: Center(
         child: Container(
-          decoration: const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            color: Colors.green,
-          ),
-          // color: Colors.green,
-          height: mobile == false ?(correntWidth * 80) / 100 : (correntWidth * 100) / 100,
-          width: mobile == false ?(correntWidth *  70) / 100 : (correntWidth *  95) / 100,
-          child: ProjectWidget(project: project[0],)),),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+              color: Colors.green,
+            ),
+            height: widget.mobile == false
+                ? (correntWidth * 80) / 100
+                : (correntWidth * 100) / 100,
+            width: widget.mobile == false
+                ? (correntWidth * 70) / 100
+                : (correntWidth * 95) / 100,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ProjectWidget(
+                  project: project[_indice],
+                ),
+                SizedBox(height: 50,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      _indice != 0 
+                      ? 
+                      IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _decrementIndice();
+                              });
+                            },
+                            icon: Icon(Icons.navigate_before))
+                      :
+                             Icon(Icons.navigate_before),
+                      
+
+
+                      _indice == 0 ? Icon(Icons.circle,color: Colors.grey[600]) :  Icon(Icons.circle,size: 15,color: Colors.grey[800]),
+                      _indice == 1 ? Icon(Icons.circle,color: Colors.grey[600]) :  Icon(Icons.circle,size: 15,color: Colors.grey[800]),
+                      _indice == 2 ? Icon(Icons.circle,color: Colors.grey[600]) :  Icon(Icons.circle,size: 15,color: Colors.grey[800]),
+                  
+                  
+                  
+                     
+                         _indice != project.length -1 
+                         ? 
+                          IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _incrementIndice();
+                              });
+                            },
+                            icon: Icon(Icons.navigate_next))
+                          :
+                            Icon(Icons.navigate_next),
+                     
+                    ],
+                  ),
+                )
+              ],
+            )),
+      ),
     );
   }
 }
---
+++ lib/widgets/nevegate_widget.dart
@@ -0,0 +1,12 @@
+// import 'package:flutter/material.dart';
+
+// class NavegateWidget extends StatelessWidget {
+//   final int indice;
+//   const NavegateWidget({super.key, required this.indice});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     ListView.builder(itemBuilder: itemBuilder)
+//     return const Placeholder();
+//   }
+// }
